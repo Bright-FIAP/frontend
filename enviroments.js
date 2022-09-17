546a9812ead886ead624a720f81f13d638b7e691
kaikeqp <kaikemice@gmail.com>
2022-09-16 21:11:27 -0300

    ajustes para pegar urls das apis

--- enviroments.js
+++ enviroments.js
@@ -1,38 +1,37 @@
-/* eslint-disable prettier/prettier */
-import Constants from 'expo-constants';
-import { Platform } from 'react-native';
+// import Constants from 'expo-constants';
+// import { Platform } from 'react-native';
 
-const localhost = Platform.OS !== 'ios' ? 'http://localhost:8080/api/v1/' : '10.0.2.2:8080';
-const apiHeroku = 'https://chefbot-api.herokuapp.com/api/v1/';
-const apiNodeRed = 'https://noderedkaike.mybluemix.net/chefbot';
+// const localhost = Platform.OS !== 'ios' ? 'http://localhost:8080/api/v1/' : '10.0.2.2:8080';
+// const apiHeroku = 'https://chefbot-api.herokuapp.com/api/v1/';
+// const apiNodeRed = 'https://noderedkaike.mybluemix.net/chefbot';
 
-const ENV = {
-  dev: {
-    API_URL: apiHeroku,
-    API_NODE_RED: apiNodeRed,
-    amplitudeApiKey: null,
-  },
-  staging: {
-    API_URL: apiHeroku,
-  },
-  prod: {
-    API_URL: apiHeroku,
-  },
-};
+// const ENV = {
+//   dev: {
+//     API_URL: apiHeroku,
+//     API_NODE_RED: apiNodeRed,
+//     amplitudeApiKey: null,
+//   },
+//   staging: {
+//     API_URL: apiHeroku,
+//   },
+//   prod: {
+//     API_URL: apiHeroku,
+//   },
+// };
 
-const getEnvVars = (env = Constants.manifest.releaseChannel) => {
-  // What is __DEV__ ?
-  // This variable is set to true when react-native is running in Dev mode.
-  // __DEV__ is true when run locally, but false when published.
-  if (__DEV__) {
-    return ENV.dev;
-  }
-  if (env === 'staging') {
-    return ENV.staging;
-  }
-  if (env === 'prod') {
-    return ENV.prod;
-  }
-};
+// const getEnvVars = (env = Constants.manifest.releaseChannel) => {
+//   // What is __DEV__ ?
+//   // This variable is set to true when react-native is running in Dev mode.
+//   // __DEV__ is true when run locally, but false when published.
+//   if (__DEV__) {
+//     return ENV.dev;
+//   }
+//   if (env === 'staging') {
+//     return ENV.staging;
+//   }
+//   if (env === 'prod') {
+//     return ENV.prod;
+//   }
+// };
 
-export default getEnvVars;
+// export default getEnvVars;
